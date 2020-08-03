0ff4276f80bed186c69d117c9f551a9a9c915ba8
“bokrystal <345549196@qq.com>
2020-08-03 11:13:19 +0800

    注释

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,13 @@
-import { Component } from '@angular/core';
 
+import { Component,OnInit} from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.less']
 })
-export class AppComponent {
-  title = 'my-ngDemo';
+export class AppComponent implements OnInit{
+    title="";
+    ngOnInit(){
+        this.title = "my-ngDemo";
+    };
 }
